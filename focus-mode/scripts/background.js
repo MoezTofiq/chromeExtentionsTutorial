4826f1f346f271f6c577bb84ccc8e116335def2f
Moeez Tofiq <moeeztofiq@gmail.com>
2024-02-02 01:39:00 +0500

    premissions and actions added to json

--- focus-mode/scripts/background.js
+++ focus-mode/scripts/background.js
@@ -1,6 +1,7 @@
 // the code below is run when the extention is installed.
 // so here we are setting the initial state for our extention
 chrome.runtime.onInstalled.addListener(() => {
+  // this is calling an action that the extention can perform
   chrome.action.setBadgeText({
     text: "OFF",
   });
